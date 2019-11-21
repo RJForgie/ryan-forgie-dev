8b1b25cf1e7ad86dced8e62e5f995ad6e921c323
Ryan <ryan.j.forgie@gmail.com>
2019-11-21 18:01:44 +0000

    add honeypot field

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -45,7 +45,9 @@ export default () => {
         data-netlify="true"
         className="w-full max-w-lg"
         onSubmit={handleSubmit}
+        netifly-honeypot="bot-field"
       >
+        <input type="hidden" name="bot-field" value="contact" />
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
