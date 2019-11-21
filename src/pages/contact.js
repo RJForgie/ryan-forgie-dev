f60ac7b9d1e87dfe0eeac63260aff1acad711577
Ryan <ryan.j.forgie@gmail.com>
2019-11-21 17:49:47 +0000

    add prevent default on form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -32,6 +32,10 @@ export default () => {
     },
   })
 
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
     <Layout>
       <form
@@ -40,6 +44,7 @@ export default () => {
         method="POST"
         data-netlify="true"
         className="w-full max-w-lg"
+        onSubmit={handleSubmit}
       >
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
