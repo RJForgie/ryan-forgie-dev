e69532df032c822be4d2fa4297c449a64495f57c
Ryan Forgie <ryan.j.forgie@gmail.com>
2019-11-26 20:10:32 +0000

    Add netifly cms (#2)
    * add basic form with styling
    
    * add formik
    
    * tidying up formik
    
    * add prevent default on form
    
    * add honeypot field
    
    * update favicon
    
    * update packages
    
    * add netifly cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-postcss`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -27,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
